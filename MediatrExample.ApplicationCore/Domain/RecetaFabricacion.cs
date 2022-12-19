0f35ed1f3129c4e8dd7875d30e0f05c6cbf2f787
Jorge Gutierrez Salgado <jgutierrez@fpc.cl>
2022-12-19 17:38:59 -0300

    inicio receta fabricación

---
+++ MediatrExample.ApplicationCore/Domain/RecetaFabricacion.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MediatrExample.ApplicationCore.Domain;
+
+[Table("RecetaFabricacion", Schema = "trzreceta")]
+
+public class RecetaFabricacion
+{
+    public int RecetaFabricacionId { get; set; }
+    public int EspecificacionTecnicaId { get; set; }
+    public string CodigoReceta { get; set; } = default!;
+    public int Version { get; set; }
+    public int Estado { get; set; }
+    public bool? AprobacionGerencia { get; set; }
+    public bool? AprobacionJefatura { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime InicioVigencia { get; set; }
+    public DateTime TerminoVigencia { get; set; }
+}
+
